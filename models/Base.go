23b66942994a8c812640d53a3d5486448cf6740c
wangpingjx <wangping.jx@gmail.com>
2017-03-22 18:42:54 +0800

    test Find()

--- models/Base.go
+++ models/Base.go
@@ -4,14 +4,14 @@ import (
     "goblin/db"
     _ "github.com/go-sql-driver/mysql"
     "log"
-    "time"
+    // "time"
 )
 
 var DB *db.DB
 var err error
 
 func init() {
-    DB, err = db.Open("mysql", "root:@tcp(127.0.0.1:3306)/sumi")
+    DB, err = db.Open("mysql", "root:@tcp(127.0.0.1:3306)/sumi?parseTime=true")
     if err != nil {
         log.Fatal(err)
     }
@@ -25,6 +25,24 @@ func init() {
     // DB.RemoveIndex(&Test{}, "name")
 
     // CRUD
-    author = &Author{Name: "测试4", Created_at: time.Now(), Updated_at: time.Now()}
-    DB.Create(author)
+    // author := &Author{Name: "测试4", Created_at: time.Now(), Updated_at: time.Now()}
+    // DB.Create(author)
+
+    author    := Author{}
+    rows, err := DB.First(&author)
+
+    // 希望达成的效果
+    // log.Printf("author name is: %v", author.Name)
+
+    // 现在的效果
+    if err != nil {
+        log.Fatal(err)
+    }
+    for rows.Next() {
+        err := rows.Scan(&author.Id, &author.Name, &author.Created_at, &author.Updated_at)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+    log.Printf("result: %v", author)
 }
